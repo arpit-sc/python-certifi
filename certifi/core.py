b498c94390d2e27fbe70a963af48d50c564345b3
arpit-sc <51862566+arpit-sc@users.noreply.github.com>
2019-08-04 03:16:20 +0530

    Update core.py

--- certifi/core.py
+++ certifi/core.py
@@ -7,14 +7,7 @@ certifi.py
 This module returns the installation location of cacert.pem.
 """
 import os
-import certifi
 
 
 def where():
-    import importlib.resources
-    with importlib.resources.open_binary(certifi, 'cacert.pem') as fh:
-        data = fh.read()
-    return data
-        
-#     reader = __loader__.get_resource_reader(__name__)
-#     return reader.open_resource('cacert.pem')
+    return os.path.join(os.getcwd(), 'cacert.pem')
